b7e487a5fa6b5abe519704c6a94c5248dd38e813
Shahriyar Muhaymeen <1905043@ugrad.cse.buet.ac.bd>
2024-01-30 22:12:43 +0600

    JWT based token and partial verification

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js"
 import { createError } from "../utils/error.js";
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 export const register = async (req, res, next)=>{
     try{
@@ -28,12 +29,12 @@ export const login = async (req, res, next)=>{
         const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
         if(!isPasswordCorrect) return next(createError(400, "wrong username or password"))
 
+        const token = jwt.sign({id:user._id, isAdmin:user.isAdmin}, process.env.JWT_SECRET)
 
-        res.status(200).json(user)
-
-        const {password, isAdmin, ...others} = user._doc;
-        res.status(200).json({...others});
+        const {password, isAdmin, ...otherDetails} = user._doc;
+        res.cookie("access_token", token,{httpOnly:true}).status(200).json({...otherDetails});
     }catch(err){
         next(err)
+    
     }
 }
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,18 @@
 import express from "express"
+import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js";
 
 const router = express.Router();
 
+//update
+router.put("/:id", updateUser);
 
+//delete
+router.delete("/:id", deleteUser);
+
+//get
+router.get("/:id", getUser);
+
+//getall
+router.get("/", getUsers);
 
 export default router;
