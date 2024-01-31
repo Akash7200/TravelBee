b5636cf2d272e1384aa0128ad3275ecd6de7d817
Shahriyar Muhaymeen <1905043@ugrad.cse.buet.ac.bd>
2024-01-31 09:41:52 +0600

    JWT verification done

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,18 +1,31 @@
 import express from "express"
 import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+// router.get("/checkauthentication", verifyToken, (req, res, next) => {
+//     res.send("You are logged in")
+// })
+
+// router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+//     res.send("You are logged in and you can delete this account")
+// })
+
+// router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+//     res.send("Hello admin, you are logged in and can delete all accounts")
+// })
+
 //update
-router.put("/:id", updateUser);
+router.put("/:id",verifyUser, updateUser);
 
 //delete
-router.delete("/:id", deleteUser);
+router.delete("/:id",verifyUser, deleteUser);
 
 //get
-router.get("/:id", getUser);
+router.get("/:id",verifyUser, getUser);
 
 //getall
-router.get("/", getUsers);
+router.get("/",verifyAdmin, getUsers);
 
 export default router;
