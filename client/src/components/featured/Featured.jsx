5a483441fb1cb14d1f08110e1bc88e6e6713a80b
Shahriyar Muhaymeen <1905043@ugrad.cse.buet.ac.bd>
2024-02-11 01:45:49 +0600

    login auth api

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -4,7 +4,6 @@ import "./featured.css"
 const Featured = () => {
 
     const { data, loading, error} = useFetch("http://localhost:8000/api/hotels/countByCity?cities=berlin,madrid,london");
-    console.log(data)
 
     return (
         <div className="featured">
