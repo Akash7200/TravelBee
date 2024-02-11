0415d3e791bffc780f7690bbeafd407c6b36a72d
Shahriyar Muhaymeen <1905043@ugrad.cse.buet.ac.bd>
2024-02-11 15:34:41 +0600

    admin logout done

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,6 @@ import Home from "./pages/home/Home";
 import Hotel from "./pages/hotel/Hotel";
 import List from "./pages/list/List";
 import Login from "./pages/login/Login";
-
 function App() {
   return (
     <BrowserRouter>
