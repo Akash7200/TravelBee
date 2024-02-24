7b0b1ae7e31575bd45a72041870b0650009e4c30
Shahriyar Muhaymeen <1905043@ugrad.cse.buet.ac.bd>
2024-02-24 12:17:30 +0600

    .

--- api/models/Review.js
+++ api/models/Review.js
@@ -27,8 +27,19 @@ const reviewSchema = new mongoose.Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now
+        default: () => {
+            // Create a new Date object with the current time
+            const currentDate = new Date();
+
+            // Adjust the timezone offset to GMT+6:00
+            const timezoneOffset = -6 * 60; // Offset is in minutes
+            const utcTimestamp = currentDate.getTime() + (currentDate.getTimezoneOffset() * 60000);
+            const gmt6Timestamp = new Date(utcTimestamp + (timezoneOffset * 60000));
+            gmt6Timestamp.setHours(gmt6Timestamp.getHours() - 6);
+            return gmt6Timestamp;
+        }
     }
-})
+    
+});
 
 export default mongoose.model('Review', reviewSchema);
