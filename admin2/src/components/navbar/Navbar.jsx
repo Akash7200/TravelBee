41b9e9fbe6a676b1b829e34eff2ce309e7157f0c
Shahriyar Muhaymeen <1905043@ugrad.cse.buet.ac.bd>
2024-03-03 13:44:58 +0600

    deploy code

--- admin2/src/components/navbar/Navbar.jsx
+++ admin2/src/components/navbar/Navbar.jsx
@@ -25,8 +25,8 @@ const Navbar = () => {
     <div className="navbar">
       <div className="wrapper">
         <div className="search">
-          <input type="text" placeholder="Search..." value={searchQuery} onChange={handleSearchChange} />
-          <SearchOutlinedIcon />
+          {/* <input type="text" placeholder="Search..." value={searchQuery} onChange={handleSearchChange} />
+          <SearchOutlinedIcon /> */}
         </div>
         <div className="items">
           <div className="item">
